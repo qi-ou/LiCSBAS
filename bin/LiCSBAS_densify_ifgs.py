cafd7986b35aeb62889957e2b7affcf0634831df
qi-ou <ouqi1005@gmail.com>
2023-05-24 17:21:50 +0100

    add options for bounds of dt

--- bin/LiCSBAS_densify_ifgs.py
+++ bin/LiCSBAS_densify_ifgs.py
@@ -33,10 +33,14 @@ def init_args():
     parser.add_argument('-s', dest='secondary_epoch_list', default=None, type=str, help="input text file containing a list of epochs to serve as secondary epochs")
     parser.add_argument('-e', dest='existing_ifg_list', default=None, type=str, help="input text file containing a list of epochs, used to avoid generating existing ifgs in the output list, or used to provide primary and secondary epochs when -p and -s are not used. ")
     parser.add_argument('-o', dest='output_ifg_list', default=None, type=str, help="output text file containing a list of epochs")
+    parser.add_argument('-l', dest='minimum_temporal_baseline', default=None, type=int, help="minimum temporal baseline in days")
+    parser.add_argument('-u', dest='maximum_temporal_baseline', default=None, type=int, help="maximum temporal baseline in days")
     args = parser.parse_args()
 
 
 if __name__ == "__main__":
+    init_args()
+
     # get epochs for generating new ifgs
     if args.primary_epoch_list and args.secondary_epoch_list:
         epoch1 = np.loadtxt(args.primary_epoch_list)
@@ -47,18 +51,21 @@ if __name__ == "__main__":
     else:
         sys.exit("no primary or secondary epoch list or no existing ifg list ")
 
+    # load list of any pre-existing ifgs
     if args.existing_ifg_list:
         existing_ifgdates = io_lib.read_ifg_list(args.existing_ifg_list)
     else:
         existing_ifgdates = []
 
+    # generate new ifg list
     ifgdates = []
     for e1 in epoch1:
         for e2 in epoch2:
             ifgdates.append(e1+"_"+e2)
 
+    # only keep those with
     dt = tools_lib.calc_temporal_baseline(ifgdates)
-    mask = np.logical_and(dt > 13, dt < 400)
+    mask = np.logical_and(dt > args.minimum_temporal_baseline, dt < args.maximum_temporal_baseline)
     ifgdates_masked = ifgdates[mask]
     to_process_ifgdates = list(set(ifgdates_masked)-set(existing_ifgdates))
 
