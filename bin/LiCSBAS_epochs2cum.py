9ce55e907dace3009596de20a0753818386f9806
qi-ou <ouqi1005@gmail.com>
2023-06-05 10:53:30 +0100

    -e

--- bin/LiCSBAS_epochs2cum.py
+++ bin/LiCSBAS_epochs2cum.py
@@ -62,7 +62,7 @@ def init_args():
     parser.add_argument('-s', dest='input_suffix', default=".sltd.geo.tif", type=str, help="suffix of gacos epochs")
     parser.add_argument('-g', dest='new_cumh5file', default="gacos_cum.h5", type=str, help="output cumulative displacement from gacos epochs")
     parser.add_argument('-c', dest='existing_cumh5file', default='TS_GEOCml10GACOS/cum.h5', type=str, help="cumulative displacement from LiCSBAS inversion to copy over meta data only")
-    parser.add_argument('-s', dest='same_epochs_as_existing_h5', default=False, action='store_true', help="only add the same epochs as in the existing h5")
+    parser.add_argument('-e', dest='same_epochs_as_existing_h5', default=False, action='store_true', help="only add the same epochs as in the existing h5")
     args = parser.parse_args()
 
 
