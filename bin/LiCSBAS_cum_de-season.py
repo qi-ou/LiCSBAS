1a34e437500760b8bec6d3b9a03fff0ba16b3939
qi-ou <ouqi1005@gmail.com>
2023-07-21 23:04:16 +0100

    ramp

--- bin/LiCSBAS_cum_de-season.py
+++ bin/LiCSBAS_cum_de-season.py
@@ -93,7 +93,7 @@ def plot_cum_grid(cum1, titles, suptitle, png):
         ax[row, col].set_title(titles[i])
     plt.suptitle(suptitle, fontsize='xx-large')
     plt.tight_layout()
-    fig.colorbar(im, ax=ax, label="Displacement, mm", fraction=0.05, pad=0.02)
+    fig.colorbar(im, ax=ax, label="Displacement, mm", fraction=0.02, pad=0.005)
     plt.savefig(png, bbox_inches='tight')
     plt.close()
 
