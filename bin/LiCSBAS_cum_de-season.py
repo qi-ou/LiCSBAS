3a3ef5125ccb2432df0c286e05cb2adf94329dce
qi-ou <ouqi1005@gmail.com>
2023-07-24 00:22:45 +0100

    plot residual

--- bin/LiCSBAS_cum_de-season.py
+++ bin/LiCSBAS_cum_de-season.py
@@ -191,6 +191,7 @@ def wls_pixel_wise(d, G, sig):
     res = np.zeros(d.shape)
 
     for i in np.arange(d.shape[1]):
+        print(i)
         if d.shape[1] > 1000 :
             if i % int(d.shape[1]/100) == 0:
                 print("   Solving {}% ...".format(i // int(d.shape[1]/10)))
